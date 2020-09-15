0e16ee40bfa6c91b0f5c3ae82a24ce293a4d4cc8
Daniel Pittman <dpdanpittman@gmail.com>
2020-09-15 01:12:33 -0700

    Update DucatsToken.sol

--- contracts/DucatsToken.sol
+++ contracts/DucatsToken.sol
@@ -550,40 +550,6 @@ contract ERC20 is Context, IERC20 {
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 }
 
-/**
- * @dev Extension of {ERC20} that allows token holders to destroy both their own
- * tokens and those that they have an allowance for, in a way that can be
- * recognized off-chain (via event analysis).
- */
-abstract contract ERC20Burnable is Context, ERC20 {
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) public virtual {
-        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "ERC20: burn amount exceeds allowance");
-
-        _approve(account, _msgSender(), decreasedAllowance);
-        _burn(account, amount);
-    }
-}
-
 contract DucatsToken is ERC20 {
 
     // In reality these events are not needed as the same information is included
@@ -614,9 +580,11 @@ contract DucatsToken is ERC20 {
     // A wallet sends Eth and receives ECD in return
     function buyToken(uint256 _amount) external payable {
         // Ensures that correct amount of Eth sent for ECD
-        // 1 ETH is set equal to 1 ECD
-        require(_amount == ((msg.value / 1 ether)), "Incorrect amount of Eth.");
+        // 0.00001 ETH is set equal to 1 ECD
+        require(_amount == ((msg.value / 0.00001 ether)), "Make sure you are sending enough Eth.");
         transferFrom(owner, msg.sender, _amount);
         emit ECDBuyEvent(owner, msg.sender, _amount);
     }
+
 }
+
